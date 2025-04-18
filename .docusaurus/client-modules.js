fb08203f285cabea8966b22579a26d6760a4e85c
jim <zijin@ualberta.ca>
2025-04-18 16:27:42 -0700

    Added team.js for build

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/zijingong/Code/CapstonePrep/CapstoneProject/rabbitory_webpage/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/zijingong/Code/CapstonePrep/CapstoneProject/rabbitory_webpage/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/zijingong/Code/CapstonePrep/CapstoneProject/rabbitory_webpage/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/zijingong/Code/CapstonePrep/CapstoneProject/rabbitory_webpage/src/css/custom.css"),
+];
