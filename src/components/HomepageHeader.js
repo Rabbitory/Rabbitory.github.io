ee2b400752442c3e3dfc36c29e2b7e85e389f7f7
larencozart <larencozart@gmail.com>
2025-04-25 12:50:44 -0500

    Add vidoe for later

--- src/components/HomepageHeader.js
+++ src/components/HomepageHeader.js
@@ -1,9 +1,17 @@
 import Link from "@docusaurus/Link";
 import Heading from "@theme/Heading";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function HomepageHeader() {
   return (
     <header className="header-section">
+      {/* <video className="header-video" autoPlay muted loop playsInline>
+        <source
+          src={useBaseUrl("/video/envelope-animation.webm")}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video> */}
       <div className="header-content">
         <Heading as="h1" className="header-title">
           Managing RabbitMQ, Made Easy.
